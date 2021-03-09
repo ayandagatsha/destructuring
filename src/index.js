dcc976d446ad4e57d6b163a6ea711d381a19763e
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-03-09 10:55:36 +0400

    v1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+const tesla = cars[0];
+tesla.TopSpeed = tesla.speedStats.topSpeed;
+tesla.TopColour = tesla.coloursByPopularity[0];
+
+const honda = cars[1];
+honda.TopSpeed = honda.speedStats.topSpeed;
+honda.TopColour = honda.coloursByPopularity[0];
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{tesla.TopSpeed}</td>
+      <td>{tesla.TopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{honda.TopSpeed}</td>
+      <td>{honda.TopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
